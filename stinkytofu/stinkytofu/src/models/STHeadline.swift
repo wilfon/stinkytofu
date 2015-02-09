03c6bc229b718952033f66d19f60e5e7ea5b2204
William Fong <fong,hoy@gmail.com>
2015-02-08 18:15:53 -0800

    models for story and headline

---
+++ stinkytofu/stinkytofu/src/models/STHeadline.swift
@@ -0,0 +1,20 @@
+//
+//  STHeadline.swift
+//  stinkytofu
+//
+//  Created by wil on 2015-02-08.
+//  Copyright (c) 2015 Mountain Special. All rights reserved.
+//
+
+import Foundation
+
+class STHeadline
+{
+    var id = 0
+    var articleId = 0
+    var title = "Title"
+    var preview = "Preview"
+    var previewImage: NSURL?
+    var timestampe = NSDate()
+    
+}
